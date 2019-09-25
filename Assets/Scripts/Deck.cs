34f2151f60b4b75d855897f73f12028487fe5c52
KJeffree <k.jeffree@gmail.com>
2019-09-25 14:52:22 +0100

    deck populates and shuffles

---
+++ Assets/Scripts/Deck.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Deck : MonoBehaviour
+{
+    [SerializeField] Card[] availableCards;
+
+    [SerializeField] List<Card> deck;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        foreach (Card card in availableCards)
+        {
+            int counter = card.count;
+            while (counter > 0)
+            {
+                deck.Add(card);
+                counter--;
+            };
+        };
+        
+        Shuffle(deck);
+    }
+
+    void Shuffle(List<Card> cards)
+    {
+        for (int i = cards.Count-1; i > 0; i--)
+        {
+            int rnd = Random.Range(0,i);
+            Card temp = cards[i];
+            cards[i] = cards[rnd];
+            cards[rnd] = temp;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
